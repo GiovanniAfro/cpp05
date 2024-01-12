d8aba58a16866250a02232cd7baf28d00127a6b6
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2024-01-12 16:38:12 +0100

    new cosine

---
+++ ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcavanna <gcavanna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 16:04:35 by gcavanna          #+#    #+#             */
+/*   Updated: 2024/01/12 16:37:54 by gcavanna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+	private:
+		const std::string _target;
+
+	public:
+		
+}
